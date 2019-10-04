bdfb3283761410e2cac869b71aa4815d72b6ecba
Valdir Borges <501161@soa.ad.pwcinternal.com>
2019-10-04 18:39:23 -0300

    30 %

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,12 +1,32 @@
+import random
 from django.db import models
 
 # Create your models here.
 
+def code_generator(size=6, chars='abcdefghijklmnopqrstuvwyxz'):
+	#new_code = ''
+	#for _ in range(size):
+	#	new_code += random.choice(chars)
+	#return new_code
+	return ''.join(random.choice(chars) for_ in range(size))
+
+
 class KirrURL(models.Model):
 	url = models.CharField(max_length=220, )
 	shortcode = models.CharField(max_length=15, unique=True)
+	updated = models.DateTimeField(auto_now=True) #everytime the is saved
+	timestamp = models.DateTimeField(auto_now_add=True) #when model was created
+	empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=True) 
 	#shortcode = models.CharField(max_length=15, null=True) Empty in database is okay
 	#shortcode = models.CharField(max_length=15, default='defaultshortcode')
+
+	def save(self, *args, **kwargs):
+		print('something')
+		self.shortcode = code_generator()
+		super(KirrURL, self).save(*args, **kwargs)
+
+	#def my_save(self):
+	#	self.save()
 
 	def __str__(self):
 		return str(self.url)
@@ -19,4 +39,5 @@ Sempre que alterar, execute
 python manage.py makemigrations
 python manage.py migrate
 
+python manage.py createsuperuser
 '''		

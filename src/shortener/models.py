6b1f3a7a83edde69169fd3c5322539690275fe48
Valdir Borges <501161@soa.ad.pwcinternal.com>
2019-10-09 18:27:18 -0300

    aula 17

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,28 +1,40 @@
-import random
 from django.db import models
+from .utils import code_generator, create_shortcode
 
 # Create your models here.
 
-def code_generator(size=6, chars='abcdefghijklmnopqrstuvwyxz'):
-	#new_code = ''
-	#for _ in range(size):
-	#	new_code += random.choice(chars)
-	#return new_code
-	return ''.join(random.choice(chars) for_ in range(size))
+class KirrURLManager(models.Manager):
+	def all(self, *args, **kwargs):
+		qs_main = super(KirrURLManager, self).all(*args, **kwargs)
+		qs = qs_main.filter(active=False)
+		return qs
 
+	def refresh_shortcodes(self):
+		qs = KirrURL.objects.filter(id__gte=1)
+		new_codes = 0
+		for q in qs:
+			q.shortcode = create_shortcode(q)
+			print(q.shortcode)
+			q.save()
+			new_codes += 1
+		return "New codes made: {i}".format(i=new_codes)
 
 class KirrURL(models.Model):
-	url = models.CharField(max_length=220, )
-	shortcode = models.CharField(max_length=15, unique=True)
-	updated = models.DateTimeField(auto_now=True) #everytime the is saved
+	url       = models.CharField(max_length=220, )
+	shortcode = models.CharField(max_length=15, default='abc', unique=True, blank=True)
+	updated   = models.DateTimeField(auto_now=True) #everytime the is saved
 	timestamp = models.DateTimeField(auto_now_add=True) #when model was created
-	empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=True) 
+	active    = models.BooleanField(default=True)
+	#empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=True) 
 	#shortcode = models.CharField(max_length=15, null=True) Empty in database is okay
 	#shortcode = models.CharField(max_length=15, default='defaultshortcode')
 
+	objects = KirrURLManager()
+	#some_random = KirrURLManager()
+
 	def save(self, *args, **kwargs):
-		print('something')
-		self.shortcode = code_generator()
+		if self.shortcode is None or self.shortcode == "":
+			self.shortcode= create_shortcode(self)
 		super(KirrURL, self).save(*args, **kwargs)
 
 	#def my_save(self):

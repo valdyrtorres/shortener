0981dfcd959d16393d66ad6eee5d7afbdc403f3d
Valdir Borges <501161@soa.ad.pwcinternal.com>
2020-01-06 17:52:47 -0300

    Aula 26 finalizada

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -6,8 +6,23 @@ from .models import KirrURL
 
 # Create your views here.
 
+def home_view_fbv(request, *args, **kwargs):
+	if request.method == "POST":
+		print(request.POST)
+
+	return render(request, "shortener/home.html", {})
+
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
+		return render(request, "shortener/home.html", {})
+
+	def post(self, request, *args, **kwargs):
+		#some_dict = {}
+		#some_dict["url"] #error
+		#some_dict.get("url", "http://spring.io") #None
+		print(request.POST)
+		print(request.POST["url"])
+		print(request.POST.get("url"))
 		return render(request, "shortener/home.html", {})
 
 class KirrCBView(View): #class based view

62f02777d3c279629dc0823ca97a9c7a7ea987f0
Valdir Borges <501161@soa.ad.pwcinternal.com>
2020-02-11 18:54:41 -0300

    encurtador completo antes do heroku

--- src/shortener/validators.py
+++ src/shortener/validators.py
@@ -3,24 +3,18 @@ from django.core.validators import URLValidator
 
 def validate_url(value):
 	url_validator = URLValidator()
-	value_1_invalid = False
-	value_2_invalid = False
+	reg_val = value 
+	if "http" in reg_val:
+		new_value = reg_val
+	else:
+		new_value = 'http://' + value 
 
 	try:
-		url_validator(value)
+		url_validator(new_value)
 	except:
-		value_1_invalid = True
-
-	value_2_url = "http://" + value
-
-	try:
-		url_validator(value_2_url)
-	except:
-		value_2_invalid = True
-
-	if value_1_invalid == False and value_2_invalid == False:
 		raise ValidationError("Invalid URL for this field")
-	return value
+
+	return new_value
 
 def validate_dot_com(value):
 	if not "com" in value:
